c73aa837cd9f00b40f169adb8a9f019d7f92a9e0
NessaAz <vanessa.azenwa@gmail.com>
2022-04-08 19:45:31 +0300

    add the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TasksComponent } from './component/tasks/tasks.component';
+import { FormComponent } from './component/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent
+    TasksComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
